81d6d8c05c039675c691f0b20a71be5ce70f0835
Jake <jake@Jakes-MacBook-Air.local>
2019-03-14 00:30:35 -0400

    Transpile JS to ensure compatability with ES5

--- http.js
+++ http.js
@@ -1,61 +1,60 @@
-function Http(){
+function Http() {
     this.get = get;
     this.post = post;
 
-    function get(url){
+    function get(url) {
         return request('GET', url);
     }
-    function post(url, data){
+    function post(url, data) {
         return request('POST', url, data);
     }
-    function request(type, url, data){
-        let http = new XMLHttpRequest();
-        let success = undefined;
-        let error = undefined;
-        let returnObj = {};
+    function request(type, url, data) {
+        var http = new XMLHttpRequest();
+        var success = undefined;
+        var error = undefined;
+        var returnObj = {};
 
-        if(type == 'GET'){
+        if (type == 'GET') {
             http.open(type, url, true);
             http.send();
-        } else if(type == 'POST'){
+        } else if (type == 'POST') {
             http.open("POST", url, true);
             http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-            let queries = [];
+            var queries = [];
             data = data || {};
 
-            for(let key in data){
+            for (var key in data) {
                 queries.push(key + '=' + encodeURIComponent(data[key] || ''));
             }
 
             http.send(queries);
-        } else{
+        } else {
             return console.error(type, 'method not supported');
         }
 
-        http.onreadystatechange = function(){
-            if(this.readyState == 4){
-                if(this.status == 200){
-                    if(success){
-                        success(http.responseText)
+        http.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    if (success) {
+                        success(http.responseText);
                     }
-                } else{
-                    if(error){
+                } else {
+                    if (error) {
                         error({
                             status: http.status,
-                            body: http.responseText,
+                            body: http.responseText
                         });
                     }
                 }
             }
         };
 
-        returnObj.then = function(cb){
+        returnObj.then = function (cb) {
             success = cb;
             return returnObj;
         };
-        
-        returnObj.catch = function(cb){
+
+        returnObj.catch = function (cb) {
             error = cb;
             return returnObj;
         };
